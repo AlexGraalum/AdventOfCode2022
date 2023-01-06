300de7c586c8da0e83d680a6f99c14fc041201fc
graalumj <alexgraalum@yahoo.com>
2023-01-06 12:35:36 -0800

    Day 6 Adjusted For Both Outputs

--- Day6/main.cpp
+++ Day6/main.cpp
@@ -9,11 +9,19 @@ int main(int argc, char** argv) {
      std::string s;
      getline(inputFile, s);
 
+     bool four = false, fourteen = false;
+
      for (int c = 0; c < s.length() - 3; c++) {
-          if (AnalyzeSubString(s.substr(c, 14)) == true) {
-               std::cout << "Packet Marker Detected After Character " << c + 14 << std::endl;
-               return 0;
+          if ((four == false) && (AnalyzeSubString(s.substr(c, 4)) == true)) {
+               std::cout << "\nFour Character Packet Marker Located After Character " << c + 4 << "\n";
+               four = true;
           }
+          if ((fourteen == false) && (AnalyzeSubString(s.substr(c, 14)) == true)) {
+               std::cout << "\nFourteen Character Packet Marker Located After Character " << c + 14 << "\n";
+               fourteen = true;
+          }
+
+          if (four && fourteen) return 0;
      }
      return 0;
 }
